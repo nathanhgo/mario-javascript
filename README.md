c776a6ea219b29b7216888f81feb888569b40c4f
Nathan Guimarães <nathanhguimaraes@gmail.com>
2022-08-13 14:19:27 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mario-javascript
  Jogo simulando "Jogo do dinossauro" feito em Javascript
+
+
+Position Relative -> Position Absolute
+Overflow Hidden
+
+animation -> Infinite e Linear
---
+++ js/script.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+const textoPontuacao = document.querySelector('.textoPontuacao')
+let pontuacao = 0
+
+const jump = () => {
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 700)
+}
+
+const loopPontuacao = setInterval(() => {
+    pontuacao++;
+    textoPontuacao.innerHTML = pontuacao
+}, 1500)
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && marioPosition <= 100 && pipePosition > 0) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`
+        mario.src = "./images/game-over.png"
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop)
+        clearInterval(loopPontuacao)
+    }
+
+}, 10)
+
+document.addEventListener('keydown', jump)
